366f2c4e9670269cb8b19a247fab2cad58c106b0
alestehlick <57234187+alestehlick@users.noreply.github.com>
2025-12-15 05:14:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 
 const CONFIG = {
   // Paste your Apps Script Web App URL here:
-  API_URL: "https://script.google.com/macros/u/1/s/AKfycbyd3wrjqMu53DzW4ov7Byt64vQOb2R2YL8I_l8-mGR_stbHE5Qi0ZiehYzm6iGZtf-52A/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyd3wrjqMu53DzW4ov7Byt64vQOb2R2YL8I_l8-mGR_stbHE5Qi0ZiehYzm6iGZtf-52A/exec",
 };
 
 const $ = (id) => document.getElementById(id);
