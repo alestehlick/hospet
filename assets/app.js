24a7525fdf14821230cf1a64af35ebe8ea660a40
alestehlick <57234187+alestehlick@users.noreply.github.com>
2025-12-15 06:53:31 +0900

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,7 @@
    ========================================================= */
 
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbysKmdf8xdb3xeQitWX4bZmJ6LNjhJb-V-xkQLjgmlN4wEbpTL1ZuGK2vuScMYXieneVA/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbyd3wrjqMu53DzW4ov7Byt64vQOb2R2YL8I_l8-mGR_stbHE5Qi0ZiehYzm6iGZtf-52A/exec",
 };
 
 const $ = (id) => document.getElementById(id);
