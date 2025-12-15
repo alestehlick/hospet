a40275ba34b046f2000bdb214e85ae0543c97fec
alestehlick <57234187+alestehlick@users.noreply.github.com>
2025-12-15 09:42:21 +0900

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
-window.CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxWN-dz9zvcI0sxYuNT0mFAmn3kSR0kGTTzD2THneTlfsVgD1uTjC8SMneffSOZPyVqlw/exec"
-};
-
+// assets/config.js
+window.CONFIG = window.CONFIG || {};
+window.CONFIG.API_URL =
+  "https://script.google.com/macros/s/AKfycbxWN-dz9zvcI0sxYuNT0mFAmn3kSR0kGTTzD2THneTlfsVgD1uTjC8SMneffSOZPyVqlw/exec";
